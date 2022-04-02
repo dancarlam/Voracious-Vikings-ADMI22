8e861d8bf47ecd661255d1ea17535b828378c49b
dancarlam <dancarlam@gmail.com>
2022-04-02 18:48:04 +0000

    add HTML

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from flask import Flask # import Flask in Python
 app = Flask(__name__) # creates an instance of a Flask app
 @app.route('/') # route called by user
 def index(): # function called by '/' route
-    return 'Hello World! This App is built using Flask.'
+    return render_template('index.html')
 app.run(host='0.0.0.0', port=8000) # starts the web app at port 8000
