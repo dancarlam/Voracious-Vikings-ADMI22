1cfad987748c11d44bf2e1ee970d83116d337eff
dancarlam <dancarlam@gmail.com>
2022-04-03 02:38:50 +0000

    add new app

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-# Dockerfile
-FROM python:alpine3.7
-COPY requirements.txt /
-RUN pip3 install -r /requirements.txt
-COPY ./app.py .
-CMD ["gunicorn"  , "-b", "0.0.0.0:8888", "app:app"]
--- app.py
+++
@@ -1,7 +0,0 @@
-# Filename: app.py
-from flask import Flask # import Flask in Python
-app = Flask(__name__) # creates an instance of a Flask app
-@app.route('/') # route called by user
-def index(): # function called by '/' route
-    return render_template('index.html')
-app.run(host='0.0.0.0', port=8000) # starts the web app at port 8000
