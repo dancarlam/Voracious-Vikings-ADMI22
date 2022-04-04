22edb1c857ccd653952e2635c0d9e2b768940288
dancarlam <68430706+dancarlam@users.noreply.github.com>
2022-04-03 23:45:29 -0400

    Update main.py
    Updated comments

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
-import datetime
+#importing libraries needed
+
 
 from flask import Flask, render_template
 
+#Creates flask app
 app = Flask(__name__)
 
-
+#creates a route for the flask app
 @app.route('/')
 def root():
-    # 
-    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
-                   datetime.datetime(2018, 1, 2, 10, 30, 0),
-                   datetime.datetime(2018, 1, 3, 11, 0, 0),
-                   ]
-
-    return render_template('index.html', times=dummy_times)
-
+    
+    # returns the final HTML template 
+    return render_template('index.html')
+    
 
 if __name__ == '__main__':
-    # Rendering HTML(turns code into an interactive page)
     
+    #127.0.0.1 = loop back port(port of own computer)
     app.run(host='127.0.0.1', port=8080, debug=True)
+    #Waits for request, port directs traffic to particular process
